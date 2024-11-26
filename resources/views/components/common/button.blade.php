f618247c38df4586937f946e548a6c61270a80fa
MD NAIMUL ISLAM <naimcse56@gmail.com>
2024-11-26 09:40:21 +0600

    btn icon

--- resources/views/components/common/button.blade.php
+++ resources/views/components/common/button.blade.php
@@ -1,8 +1,9 @@
 @props([
     'id' => '',
     'value' => '',
+    'icon' => '',
     'column' => '12',
 ])
 <div class="col-md-{{$column}} mt-3">
-    <button id="{{ $id }}" {{$attributes->class(['btn btn-sm'])->merge(['type' => 'submit'])}}>{{ $value }}</button>
+    <button id="{{ $id }}" {{$attributes->class(['btn btn-sm'])->merge(['type' => 'submit'])}}><i class="{{ $icon }}"></i> {{ $value }}</button>
 </div>
